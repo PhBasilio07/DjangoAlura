d70988704d1a6315dac8d8751daa4f9c6463c5bb
PhBasilio <pedrohbasilio2002@gmail.com>
2024-02-01 11:17:38 -0300

    Formação django Alura finalizado

--- setup/settings.py
+++ setup/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria.apps.GaleriaConfig',
+    'apps.galeria.apps.GaleriaConfig',
     'django_extensions',
-    'usuarios.apps.UsuariosConfig'
+    'apps.usuarios.apps.UsuariosConfig'
 ]
 
 MIDDLEWARE = [
@@ -137,3 +137,11 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+    
+}
